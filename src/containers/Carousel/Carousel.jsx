7c1cce4ab5b206c8e1ad27fd8aa5dfa0ed5304d3
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-02-20 17:07:36 +0000

    functionality working

--- src/containers/Carousel/Carousel.jsx
+++ src/containers/Carousel/Carousel.jsx
@@ -1,54 +1,91 @@
 import Button from "../../components/Button/Button";
-import Arrow from "../../assets/images/icons/arrow.svg"
+import arrowLeft from "../../assets/images/icons/arrow-left.svg"
+import arrowRight from "../../assets/images/icons/arrow-right.svg"
 import "./Carousel.scss"
 import CarouselItem from "../../components/CarouselItem/CarouselItem";
 import {projects} from "../../assets/data/data"
 import { useState } from "react";
 import CarouselImage from "../../components/CarouselImage/CarouselImage";
 
+
 const Carousel = () => {
     //Set the carousel to start and re-render when items change 
     const [currentProjectShown, setCurrentProjectShown] = useState(0)
+   
     //Handle l and r carousel button click 
     const onClick = (e) => {
-    //If right click and id > 4, go back to start else + 1
-        if(e.target.value === "click-right"){
-            currentProjectShown === 4 ? setCurrentProjectShown(0):setCurrentProjectShown(currentProjectShown + 1)
-            return 
+        const leftButton = document.querySelector(".carousel__button-left")
+        const rightButton = document.querySelector(".carousel__button-right")
+        //If right button click
+        if(e.target.dataset.id === "click-right"){
+        //If first item remove the inactive class to left btn
+        if(currentProjectShown === 0){
+            leftButton.classList.toggle("carousel__button--inactive")
         }
-    //If left click and id < 0, go to end else - 1
-        currentProjectShown === 0 ? setCurrentProjectShown(4):setCurrentProjectShown(currentProjectShown - 1)
+        //Get next item 
+        const nextProject = currentProjectShown + 1;
+        //If this is last item add inactive class to right btn 
+        if(nextProject === projects.length -1){
+            rightButton.classList.add("carousel__button--inactive")
+        }
+        //If it is the last project return, doing nothing 
+        if(currentProjectShown === projects.length -1){
+            return
+        }
+        //Else increment the projects 
+        setCurrentProjectShown(nextProject)
+        return; 
+        }
+        //If left click, get next project 
+        const nextProject = currentProjectShown - 1
+        //If current project shown is last, remove inactive of right btn 
+        if(currentProjectShown === projects.length-1){
+            rightButton.classList.toggle("carousel__button--inactive")
+        }
+        //If at start on nxt project, add the inactive class  
+        if(nextProject === 0){
+            leftButton.classList.add("carousel__button--inactive")
+        }
+        //If at start, return and do nothing
+        if(currentProjectShown === 0){
+            return
+        }
+        //Else increment the projects 
+        setCurrentProjectShown(nextProject);
     }
+    
     //Find index of items to be shown either of side item in focus
     const findNextCarouselNum = (currentID, arrayLength) => {
-        //If at the last item, give the second last and first
+        //If at the last item, give the first and second
         if(currentID === arrayLength - 1){
-            return [(arrayLength - 2),0]
+            return [0, 1]
         }
-        //If at the first item, give the last and the second
-        if(currentID === 0){
-            return[(arrayLength - 1),1]
+        //If at the second last item, give the last and the first. 
+        if(currentID + 2 > arrayLength -1){
+             return [arrayLength -1, 0]
         }
-        //Else give items either side 
-        return [(currentID-1),(currentID +1)]
+
+        //Else give next two 
+        return [(currentID+1),(currentID +2)]
     }
     //Destructure to get index of left and right images 
     const [leftSideImg,rightSideImg] = findNextCarouselNum(currentProjectShown, projects.length)
 
     return (
         <section className="carousel">
+            <img src="" alt="" />
             <div className="carousel__image-container">
+                <CarouselImage image={projects[currentProjectShown].image} item={"main"} title={projects[currentProjectShown].projectTitle} />
                 <CarouselImage image={projects[leftSideImg].image} item={"left"} title={projects[currentProjectShown].projectTitle} />
-                <CarouselImage image={projects[currentProjectShown].image} item={"left"} title={projects[currentProjectShown].projectTitle} />
                 <CarouselImage image={projects[rightSideImg].image} item={"right"} title={projects[currentProjectShown].projectTitle} />
             </div>
             <div className="carousel__buttons">
-                <Button classNames="carousel__button" value="click-left" onClick = {onClick}  content={<img src={Arrow} alt="" />}/>
-                <Button classNames="carousel__button" value="click-right" onClick = {onClick} content={<img src={Arrow} alt="" />}/>    
+                <Button classNames="carousel__button carousel__button-left carousel__button--inactive" onClick = {onClick}  content={<img data-id ="click-left" className ="carousel__button-image" src={arrowLeft} alt="" />}/>
+                <Button classNames="carousel__button carousel__button-right" onClick = {onClick} content={<img data-id ="click-right" className ="carousel__button-image" src={arrowRight} alt="" />}/>    
             </div>
-            <article className="carousel-item__copy">
-                <h3 className="carouse-item__header">{projects[currentProjectShown].projectTitile}</h3>
-                <p className="carousel-item__text">{projects[currentProjectShown].description}</p>
+            <article className="carousel__copy">
+                <h3 className="carousel__header">{`${projects[currentProjectShown].id}. ${projects[currentProjectShown].projectTitile}`}</h3>
+                <p className="carousel__text">{projects[currentProjectShown].description}</p>
             </article>
         </section> 
      );
