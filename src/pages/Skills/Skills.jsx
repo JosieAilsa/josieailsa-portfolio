c8ea81beeeb3f1d5ba9cfddcb325962e1cf8bbac
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-03-19 11:02:36 +0000

    working on styling

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -4,15 +4,25 @@ import "./Skills.scss"
 import backgroundflower from "../../assets/images/flowers/backgroundflower.svg"
 
 const Skills = () => {
-const skillsCardsArrJSX = skillsArr.map(({title,content, icon}) => {
-    return <SkillsCard key={title} title={title} content={content} icon={icon}/>
+const skillsCardsArrJSX1 = skillsArr.map(({title,content, icon},i) => {
+    if (i > 3) return;
+    return <SkillsCard  key={title} title={title} content={content} icon={icon}/>
+})
+const skillsCardsArrJSX2 = skillsArr.map(({title,content, icon},i) => {
+    if (i < 4) return;
+    return <SkillsCard  key={title} title={title} content={content} icon={icon}/>
 })
 
     return (
             <div id="skills"className="skills page" >
                 <h3 className="page__heading">Skills</h3>
                 <div className="skills-container">
-                {skillsCardsArrJSX}
+                <div  className="skills__col-1">
+                    {skillsCardsArrJSX1}
+                </div>
+                <div className="skills__col-2">
+                    {skillsCardsArrJSX2}
+                </div>
                 </div>
             </div>
     );
