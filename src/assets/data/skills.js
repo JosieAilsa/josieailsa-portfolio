539f4598c18852b7b319d81113740215ed322093
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-03-24 20:23:03 +0000

    fixed styling

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -15,17 +15,17 @@ icon: html,
 },
 {
     title: "CSS",
-    content:"I love design. Building and creating, accessible, responsive designs is one of the things I love most about front-end development. I use preprocessor SCSS and SASS 7-in-1 architecture in all my carouselData.",
+    content:"I love design. Building and creating, accessible, responsive designs is one of the things I love most about front-end development.",
     icon: css,
 },
 {
     title: "JavaScript",
-    content: "My favourite programming language. With it, I've started learning and applying best practices and a functional programming approach. I've also tested various carouselData with Jest and Cypress.",
+    content: "I started learning javascript 18 months ago. I'm now helping others on that same journey in my current role.",
     icon: js,
 },
 {
     title: "React",
-    content: "It took me a while to get used to, but now I can't get enough of React. I built this portfolio with it! I'm currently doing Kent C Dodd's Epic React course to develop a more advanced understanding.",
+    content: "I'm passionate about React, I built this portfolio with it! I'm currently doing Kent C Dodd's Epic React course to develop a more advanced understanding.",
     icon: react,
 
 },
@@ -36,17 +36,17 @@ icon: html,
 },
 {
     title: "Communications",
-    content: "As an ex-communications professional, and now software coach, I revel in the challenge of breaking-down complex topics making them understandable to lay-audiences.",
+    content: "As an ex-communications professional, and now software coach, I love the challenge of explaining complex topics in plain english.",
     icon: comms,
 },
 {
     title: "Content writing",
-    content: "My previous career, I managed the website of a large FTSE 100 company. I wrote everything to simple webpages to long-form technical content!",
+    content: "Managing a FTSE 100's website, I wrote everything from webpages to videos, infographics and long-form technical content!",
     icon: writing,
 },
 {
     title: "Stakeholder Management",
-    content: "Working in a comms team in a large corporate, constantly involves managing and leveraging stakeholders to release content - I managed everyone from lawyers to practising research scientists",
+    content: "I've had to manage everyone from lawyers to PhD scientists, product designers and marketing directors to create and release content.",
     icon: manage,
 },
 
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -5,11 +5,6 @@ import backgroundflower from "../../assets/images/flowers/backgroundflower.svg"
 
 const Skills = () => {
 const skillsCardsArrJSX1 = skillsArr.map(({title,content, icon},i) => {
-    if (i > 3) return;
-    return <SkillsCard  key={title} title={title} content={content} icon={icon}/>
-})
-const skillsCardsArrJSX2 = skillsArr.map(({title,content, icon},i) => {
-    if (i < 4) return;
     return <SkillsCard  key={title} title={title} content={content} icon={icon}/>
 })
 
@@ -17,12 +12,7 @@ const skillsCardsArrJSX2 = skillsArr.map(({title,content, icon},i) => {
             <div id="skills"className="skills page" >
                 <h3 className="page__heading">Skills</h3>
                 <div className="skills-container">
-                <div  className="skills__col-1">
                     {skillsCardsArrJSX1}
-                </div>
-                <div className="skills__col-2">
-                    {skillsCardsArrJSX2}
-                </div>
                 </div>
             </div>
     );
